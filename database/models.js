4dc031889ce3c69bf3e523c588551742e758d8b2
Noel Mendoza <nmendozavd@gmail.com>
2020-08-24 13:41:57 -0700

    cleaning up code, adding comments

--- database/models.js
+++ database/models.js
@@ -1,11 +1,14 @@
 const db = require('./db');
 
 module.exports = {
-    getAllSongs: (callback) => {
-        db.query('SELECT * FROM songs', callback)
-    },
+    // getAllsongs 
+    getAllSongs: (callback) => { db.query('SELECT * FROM songs', callback) },
+    // getSong by id
     getSong: (id, callback) => db.query(`SELECT * FROM songs WHERE id = ${id}`, callback),
+    // createSong using JSON object 
     createSong: ({ name, genre, artist, length, song, ranking }, callback) => db.query(`INSERT INTO songs (name, genre, artist, length, song, ranking) VALUES ("${name}", "${genre}", "${artist}", ${length}, "${song}", ${ranking})`, callback),
+    // deleteSong by id
     deleteSong: (id, callback) => db.query(`DELETE FROM songs WHERE id = ${id}`, callback),
+    // updateSong by id using JSON object
     updateSong: (id, { name, genre, artist, length, song, ranking }, callback) => db.query(`UPDATE songs SET name = "${name}", genre = "${genre}", artist = "${artist}", length = ${length}, song = "${song}", ranking = ${ranking}  WHERE id = ${id}`, callback),
 }
