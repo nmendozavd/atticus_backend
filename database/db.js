18613a259c6cf0dac7d7b66760fa7c3dfda2da12
Noel Mendoza <nmendozavd@gmail.com>
2020-08-24 13:41:16 -0700

    cleaning up code, adding comments

--- database/db.js
+++ database/db.js
@@ -1,6 +1,6 @@
-var mysql = require('mysql');
+const mysql = require('mysql');
 
-var db = mysql.createConnection({
+const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   database: 'atticus'
